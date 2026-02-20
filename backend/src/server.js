45e83f12986dff799348dc45195ada5563204703
FranzVincentTogonon26 <franzvincenttogonon@gmail.com>
2026-02-20 22:33:07 +0800

    Configure Database

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,6 +8,6 @@ const PORT = ENV.PORT || 8000;
 
 connectDB().then( () => {
     app.listen( PORT, () => {
-        console.log(`Server is running on port ${PORT}}`)
+        console.log(`Server is running on port ${PORT}`)
     })
 })
