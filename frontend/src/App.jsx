959381ab703b71761aaf8f5c97e4c791c8c18ab2
FranzVincentTogonon26 <franzvincenttogonon@gmail.com>
2026-02-22 13:56:38 +0800

    Landing Page Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,20 @@
 import { useUser } from '@clerk/clerk-react';
 import { Navigate, Route, Routes } from 'react-router';
 import HomePage from './pages/HomePage'
+import Dashboard from './pages/Dashboard';
 import ProblemsPage from './pages/ProblemsPage';
 
 function App() {
 
   const { isSignedIn, isLoaded } = useUser();
 
+  if(!isLoaded) return null;
+
   return (
     <Routes>
-      <Route path="/" element={ <HomePage /> } />
-      <Route path="/problems" element={ !isLoaded ? null : isSignedIn ? <ProblemsPage /> : <Navigate to="/" /> } />
+      <Route path="/" element={ !isSignedIn ?  <HomePage /> : <Navigate to="/dashboard" /> } />
+      <Route path="/dashboard" element={ isSignedIn ?  <Dashboard/> : <Navigate to="/" /> } />
+      <Route path="/problems" element={ isSignedIn ? <ProblemsPage /> : <Navigate to="/" /> } />
     </Routes>
   )
 }
