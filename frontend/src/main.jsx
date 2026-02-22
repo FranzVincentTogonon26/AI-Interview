9f56247db206d1b3a08504e0f1cf753685f322d9
FranzVincentTogonon26 <franzvincenttogonon@gmail.com>
2026-02-22 10:26:24 +0800

    Implementing React Routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
+import { BrowserRouter } from 'react-router' 
+import { Toaster } from 'react-hot-toast'
 import './index.css'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -12,8 +14,11 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
-    </ClerkProvider>
+    <BrowserRouter>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <Toaster toastOptions={{duration:3000}} />
+        <App />
+      </ClerkProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
